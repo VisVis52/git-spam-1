3038abcb6bf7cffb8f08a02a18568ce62eb8b7fe
Vismay <91255770+VisVis52@users.noreply.github.com>
2023-11-17 04:40:44 +0530

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -5,3 +5,5 @@ document.getElementById('button').addEventListener('click', (e) => {
     document.cookie = `name=${input};`;
     document.cookie = `date=${document.lastModified};`
 }) 
+
+//https://github.com/SamaSir2003/WTLAN/tree/main/WTLAB/Year%203/Semester%205/WT%20lab
